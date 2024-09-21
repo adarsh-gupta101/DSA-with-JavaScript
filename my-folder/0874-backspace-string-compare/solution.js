43fe36830a5e28b4bb33fd3bdaeda063b3332011
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2024-09-21 08:40:43 +0000

    [LeetCode->Github 👀] Runtime - 49 ms (85.13%), Memory - 49.8 MB (55.74%)

---
+++ my-folder/0874-backspace-string-compare/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function (s = 'ab#c', t = 'ad#c') {
+    let a = [], b = [
+
+    ]
+
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == "#") {
+            a.pop()
+        } else {
+            a.push(s[i])
+        }
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (t[i] == "#") {
+            b.pop()
+        } else {
+            b.push(t[i])
+        }
+    }
+
+    return a.join("") == b.join("")
+};
