3a0f63ee1862c409304b1e1a0f75ebe41760a830
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2024-09-23 17:48:17 +0000

    [LeetCode->Github 👀] Runtime - 67 ms (66.67%), Memory - 55.5 MB (6.00%)

--- my-folder/0713-subarray-product-less-than-k/solution.js
+++ my-folder/0713-subarray-product-less-than-k/solution.js
@@ -1,7 +1,7 @@
 var numSubarrayProductLessThanK = function (nums = [10, 5, 2, 6], k = 100) {
     if (k == 0) return 0;
 
-    let result = [];
+    let result = 0;
     let start = 0;
     let currentProduct = 1;
 
