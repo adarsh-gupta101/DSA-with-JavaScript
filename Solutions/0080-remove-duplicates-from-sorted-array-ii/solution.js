adcb30f92b1424555ac2fcd70734cacd27c43aed
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2024-09-28 10:51:50 +0000

    [LeetCode->Github 👀] Runtime - 63 ms (47.88%), Memory - 51.7 MB (52.91%)

---
+++ Solutions/0080-remove-duplicates-from-sorted-array-ii/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+
+    let i = 2;
+    let k = 2
+
+    while (i < nums.length) {
+        if(nums[i]!=nums[k-2]){
+
+        [nums[i], nums[k]] = [nums[k],nums[i]]
+        k++
+        }
+        i++
+    }
+
+
+return k
+
+};
