3030e1e9f622c28603c08a9421a6b6b0de0fa365
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2024-09-28 16:33:02 +0000

    [LeetCode->Github 👀] Runtime - 57 ms (30.35%), Memory - 48.6 MB (93.04%)

---
+++ Solutions/0100-same-tree/solution.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+
+    function DFS(p, q) {
+
+        if (p == null && q == null) return true
+        if (p == null || q == null) return false
+        if (p.val !== q.val) return false;
+
+
+
+
+        return DFS(p.left, q.left)
+            && DFS(p.right, q.right)
+
+
+
+    }
+
+    return DFS(p, q)
+
+}
