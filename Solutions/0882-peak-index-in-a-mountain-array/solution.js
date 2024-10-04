566c0d508d965fab443768bd32482141585cc90f
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2024-10-04 11:20:15 +0000

    [LeetCode->Github 👀] Runtime - 65 ms (61.51%), Memory - 59.9 MB (8.68%)

---
+++ Solutions/0882-peak-index-in-a-mountain-array/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+
+
+
+
+    let start = 0;
+    end = arr.length - 1
+
+
+    while (start < end) {
+        let mid = start + Math.floor((end - start) / 2)
+
+        if (arr[mid] > arr[mid + 1]) {
+            end = mid
+        } else {
+            start = mid + 1
+        }
+
+
+
+    }
+
+    return start
+
+};
