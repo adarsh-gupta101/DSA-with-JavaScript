9d2dbe03d7cab15f657a51f36ad225730f92bd4c
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2024-10-06 09:50:05 +0000

    [LeetCode->Github 👀] Runtime - 163 ms (57.84%), Memory - 81.7 MB (46.08%)

---
+++ Solutions/1604-least-number-of-unique-integers-after-k-removals/solution.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function (arr, k) {
+
+
+    let freq = {}
+
+
+    for (let nums of arr) {
+        freq[nums] = (freq[nums] || 0) + 1
+    }
+
+    let array=Array.from(Object.values(freq))
+
+    array.sort((a,b)=>a-b)
+
+    let length=array.length;
+
+    for(let nums of array){
+        if(k>=nums){
+            k=k-nums
+            length--
+        }
+        else{
+            return length
+        }
+    }
+
+    return length
+
+
+
+};
