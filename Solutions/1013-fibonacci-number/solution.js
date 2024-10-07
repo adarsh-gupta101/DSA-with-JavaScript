a87baac88169d0694eb3499f735829bb490afcf9
Adarsh Gupta <73733229+adarsh-gupta101@users.noreply.github.com>
2024-10-07 18:29:22 +0000

    [LeetCode->Github 👀] Runtime - 53 ms (65.08%), Memory - 48.9 MB (35.51%)

---
+++ Solutions/1013-fibonacci-number/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n, memo = {}) {
+
+
+    if (n < 2) return n
+    if (memo[n]) return memo[n]
+
+    memo[n] = fib(n - 1,memo) + fib(n - 2,memo)
+
+    return memo[n]
+
+};
